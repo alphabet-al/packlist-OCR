8f8608b6bbf42e301f5732f3dad12ea19e28a8e8
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2022-08-28 21:06:01 -0500

    082822 commit

--- project/packlist_to_txt.py
+++ project/packlist_to_txt.py
@@ -2,23 +2,29 @@ import pdfplumber
 import re
 import pandas as pd
 
-input = r'project\\122135.pdf'
+input = r'project\\90861.pdf'
+text = ''
 
 with pdfplumber.open(input) as pdf:
-    page = pdf.pages[0]
-    text = page.extract_text()
+    for page in pdf.pages:
+        text += page.extract_text()
 
-good_row_re = re.compile(r'\w+_')
+good_row_re = re.compile(r'(^[A-Z]\d{5})')
 filtered_lst = []
 
 for row in text.split('\n'):
     if good_row_re.findall(row):
-        filtered_lst.append([row.split()[1], row.split()[2], row.split()[-1]])
+        filtered_lst.append([row.split()[1], row.split()[2], int(row.split()[-1])])
 
 print(filtered_lst)
 
 df = pd.DataFrame(filtered_lst)
 df.columns = ["Project", "Part", "Qty"]
+df['Picked Qty'] = ''
 
+dflist = ['Qty']
 print(df)
 
+# Check on Quantity to see if all correct values passed. Cross Reference actual Grand Total on Packlist
+# Total = df['Qty'].sum()
+# print(Total)
